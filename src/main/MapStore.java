b7cfa5ff2da762ff8f8f4a5ae5cc8f4d131920c5
WesleyH318 <wesleyh318@gmail.com>
2026-07-10 22:12:01 -0500

    Add MapStore, using HashMap backed Store implementation

---
+++ src/main/MapStore.java
@@ -0,0 +1,30 @@
+package main;
+
+import java.util.HashMap;
+import java.util.Map;
+
+// In-memory Store implementation using HashMap
+public class MapStore implements Store{
+
+    private final Map<String, String> data = new HashMap<>();
+
+    @Override
+    public void set(String key, String value){
+        data.put(key, value);
+    }
+
+    @Override
+    public String get(String key){
+        return data.get(key);
+    }
+
+    @Override
+    public boolean delete(String key){
+        return data.remove(key) != null;
+    }
+
+    @Override
+    public int size(){
+        return data.size();
+    }
+}
